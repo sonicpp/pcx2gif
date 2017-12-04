3c756783abb2f5410c3faaf2467887b55115342a
Jan Havran <havran.jan@email.cz>
2017-12-04 20:13:50 +0100

    PCX: parse header

--- pcx.c
+++ pcx.c
@@ -9,11 +9,64 @@
  */
 
 #include <stdio.h>
+#include <assert.h>
 
 #include "pcx.h"
 
+/* PCX header */
+struct PCX_header
+{
+	uint8_t manufacturer;
+	uint8_t version;
+	uint8_t encoding;
+	uint8_t bits_per_pixel;
+	uint16_t xmin;
+	uint16_t ymin;
+	uint16_t xmax;
+	uint16_t ymax;
+	uint16_t h_dpi;
+	uint16_t v_dpi;
+	uint8_t colormap[48];
+	uint8_t reserved;
+	uint8_t planes_cnt;
+	uint16_t bytes_per_line;
+	uint16_t palette_info;
+	uint16_t h_screen_size;
+	uint16_t v_screen_size;
+	uint8_t filler[54];
+} __attribute__((packed));
+
+#define SIZE_HEADER		(sizeof(struct PCX_header))
+
+static size_t load_header(struct PCX_header *header, FILE *f_pcx)
+{
+	assert(header);
+	assert(f_pcx);
+	size_t cnt;
+
+	cnt = fread(header, 1, SIZE_HEADER, f_pcx);
+	if (cnt != SIZE_HEADER)
+		return 0;
+
+	if (header->manufacturer != 0x0A)
+		return 0;
+
+	return cnt;
+}
+
 size_t pcx_load(image_t *p_img, FILE *f_pcx)
 {
+	struct PCX_header header;
+	size_t pcx_len = 0;
+	size_t block_len = 0;
+
+	/* Parse Header */
+	if ((block_len = load_header(&header, f_pcx)) == 0) {
+		fprintf(stderr, "PCX: Invalid header\n");
+		return 0;
+	}
+	pcx_len += block_len;
+
 	/* TODO */
 	return 0;
 }
